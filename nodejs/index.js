76b5caf2635008870d1e00dbce203a477d65c532
Aaron Zhang <a3nz3.g@gmail.com>
2019-09-12 14:34:49 +0800

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,34 +1,8 @@
 'use strict';
+const database = require('scf-nodejs-serverlessdb-sdk').database;
 
 exports.main_handler = async (event, context, callback) => {
-
-    context.database = context.database || require('scf-nodejs-serverlessdb-sdk').database
-  
-    //callback mode
-    context.database().connection((err,connection)=>{
-      if(!err){
-        connection.query('select * from test',async (err,results)=>{
-          if(!err){
-            console.log('db1 query result:',results)
-          }else{
-            console.error(err)
-          }
-          //test end pool
-          await context.database().endConnection()
-        })
-      }else{
-        console.error(err)
-      }
-    })
-  
-    //async mode
-    let connection = await context.database().connection()
-    connection.query('select * from coffee',(err,results)=>{
-      console.log('db2 callback query result:',results)
-    })
-  
-    let result = await connection.queryAsync('select * from coffee') //same as connection.query
-  
-  
-    console.log('db2 query result:',result)
-  }
+  let connection = await database().connection();
+  let result = await connection.queryAsync('select * from name');
+  console.log(result);
+}
